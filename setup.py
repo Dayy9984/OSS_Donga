e8ed4ab7bcdc123c00309f06a9c84ce3b74fc961
Dayy9984 <87189372+Dayy9984@users.noreply.github.com>
2023-04-11 11:29:15 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup (
+    include_package_data = True,
+    name='dayy9984',
+    version='0.0.1',
+    description = 'oss-dev my calculator 0001',
+    author='dayy9984',
+    url = "https://github.com/Dayy9984/OSS_Donga",
+    download_url = "https://github.com/Dayy9984/OSS_Donga",
+    packages = ['mycalc'],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+)
